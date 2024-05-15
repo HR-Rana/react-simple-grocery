1a231911b774f2ab5b3ff1e5e671a38c49f39db2
HR_Rana <you@example.com>
2024-05-15 16:15:24 +0600

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import Navbar from './assets/Components/Navbar/Navbar'
 function App() {
 
 
   return (
     <>
-      home page 
+     <BrowserRouter>
+        <Navbar />
+     </BrowserRouter>
     </>
   )
 }
--- src/assets/Components/Navbar/Navbar.jsx
+++ src/assets/Components/Navbar/Navbar.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaShoppingBasket } from "react-icons/fa";
+import { MdFavorite } from "react-icons/md";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaCircleUser } from "react-icons/fa6";
+
 
 export default function Navbar() {
   return (
     <div>
         
+        <div className="navigation-bar bg-gray-200 p-5 flex items-center sticky top-0 px-5 justify-between gap-5 w-full">
+            <div className="logo">
+                <Link to={"/"} >
+                    <span className='flex items-center gap-2'><FaShoppingBasket /> <h3>Grocery</h3></span>
+                </Link>
+            </div>
+            <nav className='w-[50%] '>
+                <ul className='flex justify-between'>
+                    <li>
+                        <Link to={"/"} >Home</Link>
+                    </li>
+                    <li>
+                        <Link to={"/Shop"} >Shop</Link>
+                    </li>
+                    <li>
+                        <Link to={"/Contact"} >Contact</Link>
+                    </li>
+                    <li>
+                        <Link to={"/Contact"} className='flex items-center' ><FaShoppingCart /><sup>0</sup></Link>
+                    </li>
+                    <li>
+                        <Link to={"/Contact"} ><MdFavorite /></Link>
+                    </li>
+                    <li>
+                        <Link to={"/Contact"} ><FaCircleUser /></Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
   )
 }
