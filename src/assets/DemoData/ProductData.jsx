084e686157bf89186bd7ebf5491c2c3226160906
Rana <ariyanmahmud02019@gmail.com>
2024-05-17 10:55:05 +0600

    18 products added

--- src/assets/DemoData/ProductData.jsx
+++ src/assets/DemoData/ProductData.jsx
@@ -1,8 +1,9 @@
+// catagpry images
+
 import img1 from '../Images/product-9.png'
 import img2 from '../Images/category-2.png'
 import img3 from '../Images/deal-img2.png'
-import img4 from '../Images/SoftDrink (2).jpg'
-
+import img4 from '../Images/Drinks/drink2.png'
 
 
 export const Catagories =[
@@ -27,9 +28,56 @@ export const Catagories =[
     }
 ]
 
-export const Products=[
+
+
+import fish1 from '../Images/Fishes/Fish1.jpg';
+import fish2 from '../Images/Fishes/Fish2.jpg';
+import fish3 from '../Images/Fishes/Fish3.jpg';
+import fish4 from '../Images/Fishes/Fish4.jpg';
+import fish5 from '../Images/Fishes/Fish5.jpg';
+
+// fruites images
+
+import fruite1 from '../Images/Fruites/product-1.png';
+import fruite2 from '../Images/Fruites/product-2.png';
+import fruite3 from '../Images/Fruites/product-3.png';
+import fruite4 from '../Images/Fruites/product-4.png';
+import fruite5 from '../Images/Fruites/product-5.png';
+
+
+// soft drinks images
+import drink1 from '../Images/Fruites/product-5.png';
+import drink2 from '../Images/Fruites/product-4.png';
+import drink3 from '../Images/Fruites/product-3.png';
+import drink4 from '../Images/Fruites/product-4.png';
+import drink5 from '../Images/Fruites/product-5.png';
+
+
+
+// vegetables
+import vg1 from '../Images/Vagitables/vg1.jpg';
+import vg2 from '../Images/Vagitables/vg2.jpg';
+import vg3 from '../Images/Vagitables/vg3.jpg';
+import vg4 from '../Images/Vagitables/vg4.jpg';
+import vg5 from '../Images/Vagitables/vg5.jpg';
+
+export const AllProducts=[
     {
         id:"01",
+        name:"Tomato",
+        description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga doloremque consectetur fugiat rerum cumque sed soluta corporis, accusantium adipisci nobis nisi facilis, id in voluptas exercitationem hic sint reprehenderit unde consequuntur culpa nostrum veniam autem magnam? Debitis, mollitia porro? Quas totam quaerat amet eum assumenda eos! Quasi quos eveniet animi aspernatur adipisci error quia ex autem perferendis vero consequuntur culpa ut sequi, quam repudiandae nihil, hic fugiat sit fuga accusamus perspiciatis reiciendis distinctio corporis? Fugiat ut sunt perspiciatis ratione sit laboriosam quod labore debitis dignissimos aliquid, iure cupiditate, a delectus consequuntur voluptatum distinctio dolor molestiae? Veniam similique dolores eum quod?",
+        catagory:"Vagitables",
+        price:"250",
+        off:"30%",
+        lavel:"Popular",
+        sold:"620",
+        rating:"5",
+        stock:"220",
+        image:vg1.jpg,
+
+    }, 
+    {
+        id:"02",
         name:"",
         description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga doloremque consectetur fugiat rerum cumque sed soluta corporis, accusantium adipisci nobis nisi facilis, id in voluptas exercitationem hic sint reprehenderit unde consequuntur culpa nostrum veniam autem magnam? Debitis, mollitia porro? Quas totam quaerat amet eum assumenda eos! Quasi quos eveniet animi aspernatur adipisci error quia ex autem perferendis vero consequuntur culpa ut sequi, quam repudiandae nihil, hic fugiat sit fuga accusamus perspiciatis reiciendis distinctio corporis? Fugiat ut sunt perspiciatis ratione sit laboriosam quod labore debitis dignissimos aliquid, iure cupiditate, a delectus consequuntur voluptatum distinctio dolor molestiae? Veniam similique dolores eum quod?",
         catagory:"Vagitables",
@@ -39,22 +87,233 @@ export const Products=[
         sold:"620",
         rating:"5",
         stock:"220",
-        image:"https://images.unsplash.com/photo-1616910111011-888888888888?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050"
+        image:vg2.jpg,
 
-    }
-]
-
+    },
 
 
-export const  data =[
     {
-        id:"01",
-        name:"aboul"
-    }, {
-        id:"02",
-        name:"Haboul"
-    },{
         id:"03",
-        name:"Naboul"
-    }
+        name:"Fish1",
+        description:"",
+        catagory:"Fishes",
+        price:"250",
+        off:"30%",
+        lavel:"Popular",
+        sold:"620",
+        rating:"5",
+        stock:"42",
+        image:fish1,
+
+    },  
+    {
+        id:"04",
+        name:"Fish2",
+        description:"",
+        catagory:"Fishes",
+        price:"250",
+        off:"30%",
+        lavel:"Popular",
+        sold:"620",
+        rating:"5",
+        stock:"42",
+        image:fish2,
+
+    }, 
+    {
+        id:"05",
+        name:"Almond",
+        description:"",
+        catagory:"Fruites",
+        price:"180",
+        off:"",
+        lavel:"New",
+        sold:"302",
+        rating:"5",
+        stock:"42",
+        image:fruite2,
+
+    },
+    {
+        id:"06",
+        name:"Malta",
+        description:"",
+        catagory:"Fruites",
+        price:"180",
+        off:"",
+        lavel:"New",
+        sold:"302",
+        rating:"5",
+        stock:"42",
+        image:fruite3,
+
+    },
+    {
+        id:"07",
+        name:"Apple",
+        description:"",
+        catagory:"Fruites",
+        price:"180",
+        off:"",
+        lavel:"New",
+        sold:"302",
+        rating:"5",
+        stock:"42",
+        image:fruite4,
+
+    },
+    {
+        id:"08",
+        name:"Grape flavour",
+        description:"",
+        catagory:"SoftDrinks",
+        price:"130",
+        off:"",
+        lavel:"New",
+        sold:"3213",
+        rating:"5",
+        stock:"429",
+        image:drink4,
+
+    },  {
+        id:"09",
+        name:"Cocumber",
+        description:"",
+        catagory:"Vegetables",
+        price:"130",
+        off:"",
+        lavel:"New",
+        sold:"3213",
+        rating:"5",
+        stock:"429",
+        image:vg3,
+
+    }, 
+    {
+        id:"10",
+        name:"Grape",
+        description:"",
+        catagory:"Fruites",
+        price:"180",
+        off:"",
+        lavel:"New",
+        sold:"302",
+        rating:"5",
+        stock:"42",
+        image:fruite5,
+
+    },{
+        id:"11",
+        name:"Grape",
+        description:"",
+        catagory:"SoftDrinks",
+        price:"100",
+        off:"",
+        lavel:"Popular",
+        sold:"653",
+        rating:"5",
+        stock:"320",
+        image:drink1,
+
+    },
+    {
+        id:"12",
+        name:"Banana",
+        description:"",
+        catagory:"Fruites",
+        price:"180",
+        off:"",
+        lavel:"New",
+        sold:"302",
+        rating:"5",
+        stock:"42",
+        image:fruite1,
+
+    },  {
+        id:"13",
+        name:"Chills",
+        description:"",
+        catagory:"Vegetables",
+        price:"180",
+        off:"",
+        lavel:"New",
+        sold:"302",
+        rating:"5",
+        stock:"42",
+        image:vg5,
+
+    },  
+    {
+        id:"14",
+        name:"",
+        description:"",
+        catagory:"Fishes",
+        price:"250",
+        off:"",
+        lavel:"Popular",
+        sold:"620",
+        rating:"5",
+        stock:'0',
+        image:fish3,
+
+    }, 
+    {
+        id:"15",
+        name:"Milk flavour",
+        description:"",
+        catagory:"SoftDrinks",
+        price:"100",
+        off:"",
+        lavel:"Popular",
+        sold:"653",
+        rating:"5",
+        stock:"320",
+        image:drink2,
+
+    }, 
+    {
+        id:"16",
+        name:"",
+        description:"",
+        catagory:"Fishes",
+        price:"250",
+        off:"30%",
+        lavel:"Popular",
+        sold:"620",
+        rating:"5",
+        stock:"42",
+        image:fish4,
+
+    }, 
+    {
+        id:"17",
+        name:"",
+        description:"",
+        catagory:"Fishes",
+        price:"250",
+        off:"30%",
+        lavel:"Popular",
+        sold:"620",
+        rating:"5",
+        stock:"42",
+        image:fish5,
+
+    }, 
+    {
+        id:"18",
+        name:"Orange flavour",
+        description:"",
+        catagory:"SoftDrinks",
+        price:"100",
+        off:"",
+        lavel:"Popular",
+        sold:"653",
+        rating:"5",
+        stock:"320",
+        image:drink3,
+
+    }, 
 ]
+
+
+
