6e1c83a41b782e3cf1eabfc0fa10c7d759b864d7
Rana <ariyanmahmud02019@gmail.com>
2024-05-30 22:04:21 +0600

    chekout page done

--- src/Pages/CheckoutPage/Checkout.jsx
+++ src/Pages/CheckoutPage/Checkout.jsx
@@ -5,17 +5,17 @@ import CheckoutInvoice from './CheckoutInvoice'
 export default function Checkout() {
   return (
     <div className='checkout-page py-5'>
-        <div className='w-[80%] mx-auto'>
-             <h4>Delivery Informaiton</h4>
-            <div className="flex gap-5 justify-between">
-                <div className='Order-info w-[55%]'>
+        <div className='w-[100%]  md:w-[80%] mx-auto'>
+            <div className="block md:flex   gap-5 justify-between">
+                <div className='Order-info mb-5 md:mb-0 bg-gray-100 p-5 md:p-0 w-[100%] md:w-[55%]'>
+                     <h4>Delivery Informaiton</h4>
                     <span className='p-3'>
                         <div>
                            <CheckoutForm />
                         </div>
                     </span>
                 </div>
-                <div className='flex flex-col items-center align-middle h-[100dvh] gap-5 w-[40%]'>
+                <div className='flex flex-col items-center align-middle h-[100%] gap-5  w-[100%] md:w-[40%]'>
                    <div className='checkout-view items-center w-full bg-gray-100'>
                      <CheckoutInvoice />
                    </div>
--- src/Pages/CheckoutPage/CheckoutInvoice.jsx
+++ src/Pages/CheckoutPage/CheckoutInvoice.jsx
@@ -6,16 +6,27 @@ export default function CheckoutInvoice() {
         <div className='invoice'>
                 <h6>Checkout Invoice</h6>
             <ul>
-                <li className='flex justify-between'>
-                    <span>Quantity</span> <span>01</span>
+                <li>
+                    <span>Quantity:</span> <span>01</span>
                 </li> 
-                <li className='flex justify-between'>
-                    <span>Quantity</span> <span>01</span>
+                <li>
+                    <span>Sub-Total:</span> <span>01</span>
                 </li>
+               <span style={{margin:"20px 0"}}>
+                 <hr />
+               </span>
+               <li><span>Delivery Fee:</span> <span>0</span></li>
+               <li><span>Discount:</span> <span>0</span></li>
+               <span>
+                <hr />
+               </span>
+               <li>
+                <span>Total</span> <span>150 Tk</span>
+               </li>
             </ul>
         </div>
         <div>
-             <button className='w-full'>Checkout</button>
+             <button className='w-full'>Procced Checkout</button>
         </div>
     </div>
   )
